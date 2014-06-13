0cba22576e72338798c631670c98772067bad247
Brian Hewitt <sir.durendal@gmail.com>
2014-06-13 13:57:56 -0600

    Update euler10.py

--- euler10.py
+++ euler10.py
@@ -1,3 +1,16 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 #	Solution found at: http://stackoverflow.com/questions/9233408/project-euler-10-why-the-first-python-code-runs-much-faster-than-the-second-on written by 0x90
 sieve = [True] * 2000000 # Sieve is faster for 2M primes
 
